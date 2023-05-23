c6d21135a33cb129179fe0aeab67becf360d6f8b
jbennyhuang <j.benny.huang.developer@gmail.com>
2023-05-23 00:35:07 -0400

    update

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import { healthRouter } from "./routes/diagnostics";
 
 const app = express();
+
+app.use("/health", healthRouter);
 
 app.get("/api", (_req, res) => {
   res.send("Hello world!");
